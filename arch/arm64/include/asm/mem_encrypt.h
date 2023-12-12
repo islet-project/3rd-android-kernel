d629650067abc07b8fd76e1ae420c5a95a212e6e
Suzuki K Poulose <suzuki.poulose@arm.com>
2023-12-12 16:52:05 +0000

    arm64: Enable memory encrypt for Realms
    Use the memory encryption APIs to trigger a RSI call to request a
    transition between protected memory and shared memory (or vice versa)
    and updating the kernel's linear map of modified pages to flip the top
    bit of the IPA. This requires that block mappings are not used in the
    direct map for realm guests.
    
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Co-developed-by: Steven Price <steven.price@arm.com>
    Signed-off-by: Steven Price <steven.price@arm.com>

--- arch/arm64/include/asm/mem_encrypt.h
+++ arch/arm64/include/asm/mem_encrypt.h
@@ -2,8 +2,17 @@
 #ifndef __ASM_MEM_ENCRYPT_H
 #define __ASM_MEM_ENCRYPT_H
 
+#include <asm/rsi.h>
+
 bool mem_encrypt_active(void);
 int set_memory_encrypted(unsigned long addr, int numpages);
 int set_memory_decrypted(unsigned long addr, int numpages);
+
+int realm_register_memory_enc_ops(void);
+
+static inline bool force_dma_unencrypted(struct device *dev)
+{
+	return is_realm_world();
+}
 
 #endif	/* __ASM_MEM_ENCRYPT_H */
